da80ed8d1ddf1a34e26f606fdc66d0bb8eaec429
Khanh <knh.nguyen2002@gmail.com>
2024-01-27 17:12:28 -0600

    Added hero image

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,10 +1,17 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-        <h1>This is the main page</h1>
+      <h1 id="home-header">
+        Find And Book <br />A Magical Experience
+      </h1>
+      <div className="d-flex justify-content-center">
+        <div id="hero-img-container">
+          <img id="sky-bg" src="..//img/sky-background.jpg" alt="sky background" />
+          <img id="plane-img" src="..//img/plane-transparent.png" alt="" />
+        </div>
+      </div>
     </div>
   );
 }
