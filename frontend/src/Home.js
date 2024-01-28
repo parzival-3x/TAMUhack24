8db00519dde70136c3539564ae78068f220b3ec1
Khanh <knh.nguyen2002@gmail.com>
2024-01-28 00:54:53 -0600

    Edited page to scroll down

--- frontend/src/Home.js
+++ frontend/src/Home.js
@@ -17,11 +17,9 @@ export function Home() {
       <h1 id="home-header">
         Find And Book <br />A Magical Experience
       </h1>
-      <div className="d-flex justify-content-center">
-        <div id="hero-img-container">
-          <img id="sky-bg" src="..//img/sky-background.jpg" alt="sky background" />
-          <img id="plane-img" src="..//img/plane-transparent.png" alt="" className={`flying-image ${flyOff ? 'fly-off' : 'hovering'}`} />
-        </div>
+      <div id="hero-img-container">
+        <img id="sky-bg" src="..//img/sky-background.jpg" alt="sky background" />
+        <img id="plane-img" src="..//img/plane-transparent.png" alt="" className={`flying-image ${flyOff ? 'fly-off' : 'hovering'}`} />
       </div>
       <div className="d-flex justify-content-center">
         <button className="clickable-area" onClick={handlePlaneFlyOff}>
