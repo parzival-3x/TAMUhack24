3fb19d3ce4c7aa4bb9534b6ccb0e0155d5c4c0f4
bguzmanAA <bg193b@gmail.com>
2024-01-27 19:04:01 -0600

    validate data

--- backend/form.js
+++ backend/form.js
@@ -23,37 +23,36 @@ function hasValue(input, message) {
 	return showSuccess(input);
 }
 
-function validateEmail(input, requiredMsg, invalidMsg) {
+function validateAirport(input, message) {
 	// check if the value is not empty
-	if (!hasValue(input, requiredMsg)) {
+	if (!hasValue(input)) {
 		return false;
 	}
-	// validate email format
-	const emailRegex =
-		/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
-
-	const email = input.value.trim();
+  //if /airports?code=<IATA-CODE>
+  // returns 404, return "please enter a valid code"
+	
 	if (!emailRegex.test(email)) {
-		return showError(input, invalidMsg);
+		return showError(input, message);
 	}
 	return true;
 }
 
 const form = document.querySelector("#signup");
 
-const NAME_REQUIRED = "Please enter your name";
-const EMAIL_REQUIRED = "Please enter your email";
-const EMAIL_INVALID = "Please enter a correct email address format";
+const AIRPORT_REQUIRED = "Please enter a valid airport code";
 
 form.addEventListener("submit", function (event) {
 	// stop form submission
 	event.preventDefault();
 
 	// validate the form
-	let nameValid = hasValue(form.elements["name"], NAME_REQUIRED);
-	let emailValid = validateEmail(form.elements["email"], EMAIL_REQUIRED, EMAIL_INVALID);
+	let originValid = validateAirport(form.elements["start"], AIRPORT_REQUIRED);
+  let endpointValid = validateAirport(form.elements["end"], AIRPORT_REQUIRED);
+  let connectionValid = hasValue(form.elements["connect"]);
+  let leaveValid = hasValue(form.elements["leave"]);
+	let arriveemailValid = hasValue(form.elements["arrive"]);
 	// if valid, submit the form.
-	if (nameValid && emailValid) {
+	if (originValid && endpointValid && connectionValid && leaveValid && arriveemailValid) {
 		alert("Demo only. No form was posted.");
 	}
 });
