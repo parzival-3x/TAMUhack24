ee576ab03d6028d439b3f0c47bcb4197b2ac72a3
bguzmanAA <bg193b@gmail.com>
2024-01-28 10:24:40 -0600

    Creates algos.py

---
+++ backend/algos.py
@@ -0,0 +1,13 @@
+from scipy.sparse import csr_matrix
+from scipy.sparse.csgraph import dijkstra
+graph = [
+[0, 1, 2, 0],
+[0, 0, 0, 1],
+[0, 0, 0, 3],
+[0, 0, 0, 0]
+]
+graph = csr_matrix(graph)
+print(graph)
+dist_matrix, predecessors = dijkstra(csgraph=graph, directed=False, indices=0, return_predecessors=True)
+dist_matrix
+predecessors
